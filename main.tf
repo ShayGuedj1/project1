382cb24488462248fdc742b2b230cebe2894633b
ShayGuedj1 <shayguedj1@gmail.com>
2024-04-27 21:00:41 +0300

    .

--- main.tf
+++ main.tf
@@ -15,6 +15,13 @@ resource "aws_instance" "project1" {
   }
 security_groups = [var.security_groups["docker_sg"]]
 
+connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Update with appropriate username
+    private_key = file("~/.ssh/id_rsa")  # Path to your private key
+    host        = self.public_ip  # Use the public IP of the instance
+  }
+
 provisioner "file" {
     source      = "~/.ssh/id_rsa.pub"  # Path to your local public key
     destination = "/tmp/my-public-key.pub"  # Temporary location on the instance
