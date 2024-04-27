ba8e772a639c4f68c25c7c30f3125d9d692da786
ShayGeudj1 <shayguedj1@gmail.com>
2024-04-27 10:24:30 +0000

    multi-ec2

--- main.tf
+++ main.tf
@@ -2,11 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
+
 resource "aws_instance" "project1" {
+  count         = var.instance_count
   ami           = var.amis["20.04"]
   instance_type = var.instance_types[0]
+  key_name      = "projects"
   tags = {
-    Name = "web-server"
+    Name = "web-server-${count.index + 1}"
   }
 security_groups = [var.security_groups["docker_sg"]]
 }
@@ -17,5 +24,5 @@ security_groups = [var.security_groups["docker_sg"]]
 
 
 output "instance-ip" {
-  value = aws_instance.project1.public_ip
+  value = [for instance in aws_instance.project1 : instance.public_ip]
 }
