cfe1fd7df1e19de0835e9bfa2f049e9f5dd749a6
ShayGuedj1 <shayguedj1@gmail.com>
2024-04-29 14:28:11 +0300

    .

--- main.tf
+++ main.tf
@@ -2,10 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 resource "aws_instance" "project1" {
-  count         = /*var.instance_count*/1
+#  count         = var.instance_count
   ami           = var.amis["20.04"]
   instance_type = var.instance_types[0]
   associate_public_ip_address = true  # Assign a public IP to this instance
