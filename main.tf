21088d93cd3d2815e0c3e10f5ef4352a878c7695
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-01 20:18:06 +0300

    .

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "project1" {
-  count         = var.instance_count
+  /*count         = var.instance_count*/
   ami           = var.amis["20.04"]
   instance_type = var.instance_types[0]
   associate_public_ip_address = true  # Assign a public IP to this instance
   key_name      = "projects"
   tags = {
-    Name = "web-server-${count.index + 1}"
+    Name = "web-server"/*-${count.index + 1}"*/
   }
 security_groups = [var.security_groups["docker_sg"]]
 
