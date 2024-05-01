924d678e7164d75a9c7a9395c6758b868083beb8
ShayGuedj1 <shayguedj1@gmail.com>
2024-05-01 20:15:32 +0300

    .

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "project1" {
   associate_public_ip_address = true  # Assign a public IP to this instance
   key_name      = "projects"
   tags = {
-    Name = "web-server"/*-${count.index + 1}"*/
+    Name = "web-server"-${count.index + 1}
   }
 security_groups = [var.security_groups["docker_sg"]]
 
@@ -30,10 +30,7 @@ provisioner "remote-exec" {
       "sudo mkdir -p /home/ubuntu/.ssh",  # Create .ssh directory if it doesn't exist
       "sudo cp /tmp/my-public-key.pub /home/ubuntu/.ssh/authorized_keys",  # Copy public key to authorized_keys
       "sudo chown -R ubuntu:ubuntu /home/ubuntu/.ssh",  # Change ownership to ubuntu user
-      "sudo chmod 600 /home/ubuntu/.ssh/authorized_keys",  # Set correct permissions on authorized_keys
-      #"sudo apt update",
-      #"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      #"echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release #-cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+      "sudo chmod 600 /home/ubuntu/.ssh/authorized_keys"
     ]
   }
 
